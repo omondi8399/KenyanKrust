849c8e2043416de4229c42d9ade4196bb69fe94c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-28 17:42:57 +0300

    Updated CartItem component

--- src/app/components/CartItem.js
+++ src/app/components/CartItem.js
@@ -1,5 +1,70 @@
+import Image from "next/image";
+import { BiPlus } from "react-icons/bi";
+import { BiMinus } from "react-icons/bi";
+import { IoCloseOutline } from "react-icons/io5";
+
+
 const CartItem = () => {
-  return <div>CartItem</div>;
-};
+  return (
+    <div className="select-none">
+      <div className="flex gap-x-4 mb-2">
+        {/* image */}
+        <div className="flex justify-center items-center">
+          <Image src={pizza.image} width={90} height={90} alt='' />
+        </div>
+        {/* pizza info */}
+        <div className="flex-1 flex flex-col gap-y-1">
+          {/* name */}
+          <div className="text-lg capitalize font-bold">{pizza.name}</div>
+          <div className="flex flex-col gap-y-1">
+            {/* crust */}
+            <div className="capitalize font-medium text-[15px]">{pizza.crust} crust</div>
+            {/* size */}
+            <div className="flex items-center gap-x-1">{pizza.size} size</div>
+            {/* quantity controls */}
+            <div className="flex items-center gap-x-1">
+              {/* decrease quantity */}
+              <div className="w-[18px] h-[18px] flex justify-center items-center cursor-pointer text-white 
+              gradient rounded-full">
+                <BiMinus />
+              </div>
+              {/* pizza amount */}
+              <div className="font-semibold flex flex-1 max-w-[30px] justify-center items-center text-sm">1</div>
+              {/* increase quantity */}
+              <div className="w-[18px] h-[18px] flex justify-center items-center cursor-pointer text-white 
+              gradient rounded-full">
+                <BiPlus />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between">
+          {/* remove item */}
+          <div className="text-2xl flex justify-center items-center self-end cursor-pointer hover:scale-110 duration-100
+          transition-all text-orange">
+            <IoCloseOutline />
+          </div>
+          {/* price  */}
+          <div>
+            <span className="text-[17px] font-medium font-robotoCondensed">${parseFloat(pizza.price * pizza.amount).toFixed(2)} </span>
+          </div>
+        </div>
+      </div>
+      {/* toppings */}
+        <div className="flex flex-wrap items-center gap-3 p-6 border-b border-black/10 ">
+          <div className="font-semibold">
+            Toppings: {pizza.additionalTopping.length === 0 && 'None'} 
+          </div>
+          {pizza.additionalTopping.map((topping, index) => {
+            return (
+              <div className="capitalize text-sm gradient font-medium px-3 py-1 rounded-full leading-none"
+                key={index}>{topping.name}</div>
+            )
+          })}
+        </div>
+    </div>
+  )
+
+}
 
 export default CartItem;
