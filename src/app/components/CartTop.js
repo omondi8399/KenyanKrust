e2bda329aaa377c4d81bf5075c40aa30f14ad3f1
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-28 17:40:43 +0300

    Updated CartTop component

--- src/app/components/CartTop.js
+++ src/app/components/CartTop.js
@@ -1,5 +1,22 @@
+import React, { useContext } from 'react'
+
+import { IoCloseOutline } from 'react-icons/io5'
+
+import { CartContext } from '../context/CartContext'
+
 const CartTop = () => {
-  return <div>CartTop</div>;
+  const { setIsOpen } = useContext(CartContext)
+
+  return (
+    <div className='w-full h-20 border-b flex items-center justify-between px-10'>
+      {/* shopping bag text */}
+      <div className='font-semibold'>Shopping Bag(3)</div>
+    {/* close icon */}
+    <div onClick={() => setIsOpen(false)} className='cursor-pointer group' >
+      <IoCloseOutline className='text-3xl group-hover:sclae-110 duration-300 transition-all' />
+    </div>
+    </div>
+  )
 };
 
 export default CartTop;
