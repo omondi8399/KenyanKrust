e712cccd69c27f69a2e63dd270a6d44116210858
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-28 17:42:09 +0300

    Updated CartMobile component

--- src/app/components/CartMobile.js
+++ src/app/components/CartMobile.js
@@ -1,5 +1,33 @@
+'use client'
+
+import { useContext } from 'react'
+import { CartContext } from '../context/CartContext'
+import CartTop from './CartTop'
+import CartBottom from './CartBottom'
+import CartItem from './CartItem'
+
 const CartMobile = () => {
-  return <div>CartMobile</div>;
+ const { cart, isOpen } = useContext(CartContext)
+  return (
+    <div className={`${isOpen ? 'bottom-0' : '-bottom-full'} bg-white fixed w-full h-full left-0
+    z-20 transition-all duration-300 flex flex-col lg:hidden`}>
+      {/* top */}
+      <CartTop />
+      {/* cart items */}
+      <div className={`px-4 flex flex-col gap-y-4 py-2 mr-4 mt-8 h-[60vh] overflow-y-scroll
+      scrollbar-thin scrollbar-thumb-secondary ${
+        cart.length >= 3
+          ? 'scrollbar-track-black/10'
+          : 'scrollbar-track-transparent'
+      }`}>
+        {cart?.map((pizza, index) => {
+          return <CartItem pizza={pizza} key={index} />
+        })}
+      </div>
+      {/* cart bottom */}
+      <CartBottom />
+    </div>
+  )
 };
 
 export default CartMobile;
