ce520861db97eb085ca0d86ae11fd7762efdf2a0
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-29 14:24:45 +0300

    Updated Footer component

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,5 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import { FaYoutube, FaFacebook, FaInstagram, FaPinterest } from 'react-icons/fa'
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-primary bg-pattern py-16'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col items-center gap-y-6 justify-center'>
+          {/* logo */}
+          <Link href={'#'}>
+            <Image src={'/public/logo.svg'} width={180} height={180} alt='' />
+          </Link>
+          {/* social icons */}
+          <div className='flex gap-x-6 text-xl text-white'>
+            <Link href={'#'}>
+              <FaYoutube />
+            </Link>
+            <Link href={'#'}>
+              <FaFacebook />
+            </Link>
+            <Link href={'#'}>
+              <FaInstagram />
+            </Link>
+            <Link href={'#'}>
+              <FaPinterest />
+            </Link>
+          </div>
+          {/* copyright */}
+          <div className='text-white font-medium'>Copyright &copy; KenyanKrust 2023. All rights reserved.</div>
+        </div>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
