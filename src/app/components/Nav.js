dbe14a9f9287d185a87f583fadf7298294ef88ad
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-29 10:41:10 +0300

    Updated Nav component

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -6,7 +6,7 @@ import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 
 const Nav = () => {
-  const { isOpen, setIsOpen } = useContext(CartContext)
+  const { isOpen, setIsOpen, itemAmount } = useContext(CartContext)
   return (
     <nav className="absolute w-full py-8 bg-pink-50/30">
       <div className="container mx-auto flex flex-col lg:flex-row gap-y-3 justify-between items-center">
@@ -29,7 +29,7 @@ const Nav = () => {
             <Image src={'bag.svg'} width={38} height={38} alt="" />
             {/* amount */}
             <div className="bg-tertiary w-6 h-6 rounded-full text-white flex justify-center items-center 
-            text-[13px] font-robotoCondensed absolute -bottom-2 -right-1">3</div>
+            text-[13px] font-robotoCondensed absolute -bottom-2 -right-1">{itemAmount}</div>
           </div>
         </div>
       </div>
